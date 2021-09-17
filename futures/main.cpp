bd932e28a49320c691b68df096661558a82d5dda
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-09-17 12:02:09 +0200

    async + future

--- futures/main.cpp
+++ futures/main.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <vector>
 #include <random>
+#include <future>
 
 using namespace std::literals;
 
@@ -33,6 +34,40 @@ void save_to_file(const std::string& filename)
     std::cout << "File saved: " << filename << std::endl;
 }
 
+void wtf()
+{
+    auto f1 = std::async(std::launch::async, &save_to_file, "Content1");
+    auto f2 = std::async(std::launch::async, &save_to_file, "Content2");
+    auto f3 = std::async(std::launch::async, &save_to_file, "Content3");
+    auto f4 = std::async(std::launch::async, &save_to_file, "Content4");
+}
+
 int main()
 {
+    std::future<int> f1 = std::async(std::launch::async, &calculate_square, 9);
+    std::future<int> f2 = std::async(std::launch::async, &calculate_square, 8);
+    std::future<void> f3 = std::async(std::launch::async, &save_to_file, "Some content");
+
+    while (f2.wait_for(250ms) != std::future_status::ready)
+    {
+        std::cout << "I'm still waiting for f2" << std::endl;
+    }
+
+    try
+    {
+        int result1 = f1.get();
+        std::cout << "14*14 = " << result1 << "\n";
+    }
+    catch(const std::runtime_error& excpt)
+    {
+        std::cout << "Caught an exception: " << excpt.what() << "\n";
+    }
+    
+    std::cout << "8*8 = " << f2.get() << std::endl;
+
+    f3.wait();
+
+    std::cout << "File is saved..." << std::endl;
+
+    wtf();
 }
