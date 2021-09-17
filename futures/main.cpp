96c887d240e26e4fb11d32f19c7d5f04083e8a3f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-09-17 13:41:05 +0200

    packaged_task + future

--- futures/main.cpp
+++ futures/main.cpp
@@ -42,9 +42,9 @@ void wtf()
     auto f4 = std::async(std::launch::async, &save_to_file, "Content4");
 }
 
-int main()
+void using_async()
 {
-    std::future<int> f1 = std::async(std::launch::async, &calculate_square, 9);
+        std::future<int> f1 = std::async(std::launch::async, &calculate_square, 9);
     std::future<int> f2 = std::async(std::launch::async, &calculate_square, 8);
     std::future<void> f3 = std::async(std::launch::async, &save_to_file, "Some content");
 
@@ -70,4 +70,113 @@ int main()
     std::cout << "File is saved..." << std::endl;
 
     wtf();
+}
+
+void foo(int&)
+{
+}
+
+void foo(int&&)
+{
+}
+
+template <typename T>
+void bar(T&&)
+{}
+
+void using_foo()
+{
+    int x = 10;
+    foo(x); // foo(int&)
+    foo(10); // foo(int&&)
+
+    bar(x); // bar(int&)
+    bar(10); // bar(int&&)
+}
+
+template <typename Callable>
+auto launch_async(Callable&& callable)
+{
+    using ResultT = decltype(callable());
+    
+    std::packaged_task<ResultT()> pt{std::forward<Callable>(callable)};
+    std::future<ResultT> f = pt.get_future();
+
+    std::thread thd{std::move(pt)};
+    thd.detach();
+
+    return f;
+}
+
+void no_wtf()
+{
+    launch_async([] { save_to_file("Content1"); });
+    launch_async([] { save_to_file("Content2"); });
+    launch_async([] { save_to_file("Content3"); });
+    auto f = launch_async([] { save_to_file("Content4"); });
+    f.wait();
+}
+
+class SquareCalculator
+{
+    std::promise<int> promise_;
+public:
+    void calculate(int x)
+    {
+        try
+        {
+            int result = calculate_square(x);
+            promise_.set_value(result);
+        }
+        catch(...)
+        {
+            promise_.set_exception(std::current_exception());
+        }
+    }
+
+    std::future<int> get_future()
+    {
+        return promise_.get_future();
+    }
+};
+
+void using_promise()
+{
+    SquareCalculator calc;
+
+    std::future<int> f = calc.get_future();
+    std::shared_future<int> shared_f = f.share();
+
+    std::thread consumer_thd{[shared_f] {
+        std::cout << "Consuming: " << shared_f.get() << std::endl;
+    }};
+
+    std::thread thd{[&] { calc.calculate(11); }};
+
+    std::cout << "11*11 = " << shared_f.get() << std::endl;
+
+    thd.join();
+    consumer_thd.join();
+}
+
+int main()
+{
+    std::packaged_task<int()> pt1{[] { return calculate_square(16); }};
+    std::packaged_task<int(int)> pt2{&calculate_square};
+    
+    std::future<int> f1 = pt1.get_future();
+    std::future<int> f2 = pt2.get_future();
+
+    std::thread thd1{std::move(pt1)};
+    std::thread thd2{std::move(pt2), 7};
+
+    std::cout << "16*16 = " << f1.get() << std::endl;
+    std::cout << "7*7 = " << f2.get() << std::endl;
+
+    thd1.join();
+    thd2.join();
+
+    no_wtf();
+
+    using_promise();
 }
